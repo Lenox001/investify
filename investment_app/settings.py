5e5a49c093288e07b5a1ad9d64e38627dae3e2db
GreenCoder <132973967+Lenox001@users.noreply.github.com>
2024-10-14 10:06:55 +0300

    Update settings.py

--- investment_app/settings.py
+++ investment_app/settings.py
@@ -20,8 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=6)bccvzugyoy)*al4r6@chp$+3#0(2m%#^&ryecdnlvkj&s6='
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
